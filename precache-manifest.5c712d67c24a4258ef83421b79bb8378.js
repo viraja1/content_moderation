c8783df7dc91bf035c9e2aa67aba0d076607450e
viraj <anchan.viraj@gmail.com>
2019-12-11 02:25:27 +0530

    Updates

---
+++ precache-manifest.5c712d67c24a4258ef83421b79bb8378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c90f06e103838e72207eefd9e5afb12",
+    "url": "/content_moderation/index.html"
+  },
+  {
+    "revision": "2e116ccccb0755a7383e",
+    "url": "/content_moderation/static/css/main.bbc8fbd7.chunk.css"
+  },
+  {
+    "revision": "467a3764fd2c7f0e18b0",
+    "url": "/content_moderation/static/js/2.6919013e.chunk.js"
+  },
+  {
+    "revision": "2e116ccccb0755a7383e",
+    "url": "/content_moderation/static/js/main.833412f9.chunk.js"
+  },
+  {
+    "revision": "43fd7ab796ee5542ce04",
+    "url": "/content_moderation/static/js/runtime-main.3288dff3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/content_moderation/precache-manifest.5c712d67c24a4258ef83421b79bb8378.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/content_moderation/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
